d39b698295f32a4cf1ecc857707af282e85e6583
ikefix <omekeikechukwu877@gmail.com>
2023-09-30 15:51:07 -0700

    complete the Oauth funcinality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -17,7 +17,7 @@ export const ForgotPassword = () => {
           alt="key"  className='w-full rounded-2xl'/>
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form action="">
+          <form action="" onSubmit="">
             <div className=' relative mb-4'>
               <input className='w-full px-4 py-2 text-xl mb-5 text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'type="email" id="email" value={email} onChange={onChange} placeholder='Email address'/>
              
