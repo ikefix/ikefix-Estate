5235c9cf29d8302cf5aaa5dead6fb7f0f3a06fbf
ikefix <omekeikechukwu877@gmail.com>
2023-10-01 10:31:17 -0700

    completed the sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
 import  {useState} from 'react';
 import {AiFillEyeInvisible, AiFillEye} from 'react-icons/ai';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import { OAuth } from '../component/OAuth';
+import {  getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import {toast} from 'react-toastify'
 
 export const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
-    email:" ", password:"",
+    email:"", password:"",
   });
   const {email, password} = formData; 
+  const navigate = useNavigate();
   function onChange(e){
     setFormData((prevState)=>({
         ...prevState, 
         [e.target.id]: e.target.value,
     }))
   }
+  async function onSubmit(e){
+  e.preventDefault()
+
+  try {
+    const auth = getAuth()
+    const userCredential = await signInWithEmailAndPassword(auth, email, password)
+    if(userCredential.user){
+      navigate("/")
+    }
+  } catch (error) {
+    toast.error("wrong user credentials")
+    
+  }
+}
   return (
     <section className='text-center text-3sl mb-12 mt-6 font-bold '>
       <h1>Sign in</h1>
@@ -25,7 +42,7 @@ export const SignIn = () => {
           alt="key"  className='w-full rounded-2xl'/>
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form action="">
+          <form onSubmit={onSubmit} >
             <div className=' relative mb-4'>
               <input className='w-full px-4 py-2 text-xl mb-5 text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'type="email" id="email" value={email} onChange={onChange} placeholder='Email address'/>
               <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'type={showPassword ? "text" : "password"} id="password" value={password} onChange={onChange} placeholder='password'/>
@@ -48,7 +65,6 @@ export const SignIn = () => {
 
         </div>
       </div>
-    
     </section>
   )
 }
